1455e935a10e7ac2badacde6d08db611224ca9ff
Shivani Sahu <130729441+Shivanisahu0401@users.noreply.github.com>
2024-06-30 11:35:18 +0530

    update signup endpoint API

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -9,7 +9,7 @@ const Signup = () => {
         if(!email && !password){
             return;
         }
-        const res = await fetch('http://localhost:1600/api/v1/auth/signup', {
+        const res = await fetch('https://kiet-backend.onrender.com/api/v1/auth/signup', {
             headers: {
                 "Content-Type": "application/json",
             },
